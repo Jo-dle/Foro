2aefbc232d0cf1703a5ce8386caa88a72de5f938
Nefta <neftaliespinorodriguez@gmail.com>
2025-07-24 09:52:49 +0100

    Error gitignore

---
+++ controladores/c.login.php
@@ -0,0 +1,20 @@
+<?php
+include_once "./dbconnect.php";
+session_start();
+
+if($_SERVER['REQUEST_METOHD'] === 'GET' && isset($_GET ["entrar"])){
+    $email = sanitizar($conexion, $_REQUEST['correo']);
+    $pass = hash("sha256", sanitizar($conexion, $_REQUEST['clave']));
+
+    try{
+        $query = "SELECT * FROM usuarios WHERE correo='$email' and clave='$pass'";
+        $resultset = mysqli_query($conexion, $query);
+        $row = $resultset->fetch_assoc();
+        if($row){
+            $_SESSION["id"] = $row["id"];
+            $_SESSION["nombre"] = $row["nombre"];
+            $_SESSION["correo"] = $row["correo"];
+            header("location: index.php")
+        }
+    }
+}
---
+++ login.php
@@ -0,0 +1,13 @@
+<?php 
+include_once "./controladores/c.login.php"
+?>
+
+<!DOCTYPE html>
+<html lang="es">
+    <head>
+    </head>
+    
+    <body>
+    </body>
+
+</html>
