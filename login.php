8540a785559067729a272bbd247037d0f3f59fa0
Jo-dle <jramoszurita@gmail.com>
2025-07-25 09:03:44 +0100

    mejora login y css de neftali

--- login.php
+++ login.php
@@ -5,29 +5,91 @@ include_once "./controladores/c.login.php";
 <!DOCTYPE html>
 <html lang="es">
     <head>
-        <title> Inicio de sesíon| Foro </title>
+        <title> Inicio de sesión | Foro </title>
+        <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+    <meta name="title" content="AdminLTE 4 | Login Page v2" />
+    <meta name="author" content="ColorlibHQ" />
+    <meta
+      name="description"
+      content="AdminLTE is a Free Bootstrap 5 Admin Dashboard, 30 example pages using Vanilla JS."
+    />
+    <meta
+      name="keywords"
+      content="bootstrap 5, bootstrap, bootstrap 5 admin dashboard, bootstrap 5 dashboard, bootstrap 5 charts, bootstrap 5 calendar, bootstrap 5 datepicker, bootstrap 5 tables, bootstrap 5 datatable, vanilla js datatable, colorlibhq, colorlibhq dashboard, colorlibhq admin dashboard"
+    />
+    <link
+      rel="stylesheet"
+      href="https://cdn.jsdelivr.net/npm/@fontsource/source-sans-3@5.0.12/index.css"
+      integrity="sha256-tXJfXfp6Ewt1ilPzLDtQnJV4hclT9XuaZUKyUvmyr+Q="
+      crossorigin="anonymous"
+    />
+    <link
+      rel="stylesheet"
+      href="https://cdn.jsdelivr.net/npm/overlayscrollbars@2.10.1/styles/overlayscrollbars.min.css"
+      integrity="sha256-tZHrRjVqNSRyWg2wbppGnT833E/Ys0DHWGwT04GiqQg="
+      crossorigin="anonymous"
+    />
+    <link
+      rel="stylesheet"
+      href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.11.3/font/bootstrap-icons.min.css"
+      integrity="sha256-9kPW/n5nn53j4WMRYAxe9c1rCY96Oogo/MKSVdKzPmI="
+      crossorigin="anonymous"
+    />
+    <link rel="stylesheet" href="./css/adminlte.css" />
     </head>
     
     <body class="login-page bg-body-secondary">
-        <div class="">
-            <div class="">
+        <div class="login-box">
+            <div class="card card-outline card-primary">
                 <div class="card-header">
-                    <a class="">
-                        <h1 class="">Hola!</b></h1>
+                    <a class="link-dark text-center">
+                        <h1 class="mb-0">¡Hola!</h1>
                     </a>
-            </div>
-            <div class="">
-                <p class="login-box-msg">Inicia sesión para vomenzar</p>
-                <form method="get">
-                    <div class="input-group mb-1">
-                        <div class="form-floating">
-                            <input id="loginEmail" name="email" type="email" class="form-control" value="" placeholder="example@example.com"/>
-                            <label for ="loginEmail">Email</label>
+                </div>
+                <div class="card-body login-card-body">
+                    <p class="login-box-msg">Inicia sesión para comenzar</p>
+                    <form method="get">
+                        <div class="input-group mb-1">
+                            <div class="form-floating">
+                                <input id="loginEmail" name="correo" type="email" class="form-control" value="" placeholder="example@example.com"/>
+                                <label for="loginEmail">Email</label>
+                            </div>
+                            <div class="input-group-text"><span class="bi bi-envelope"></span></div>
                         </div>
-                        <div class="input-group-text"><span class="bi bi-envelope"></span></div>
-                    </div>
-            </div>
-        </div>
-    </body>
+                        <div class="input-group mb-1">
+                            <div class="form-floating">
+                                <input id="loginPass" name="clave" type="password" class="form-control" placeholder=""/>
+                                <label for="loginPass">Contraseña</label>
+                            </div>
+                            <div class="input-group-text"><span class="bi bi-lock-fill"></span></div>
+                        </div>
+                        <div class="row">
+                            <div class="col-8 d-inline-flex align-items-center">
+                                <div class="form-check">
+                                    <input class="form-check-input" type="checkbox" value="" id="flexCheckDefault" />
+                                    <label class="form-check-label" for="flexCheckDefault"> Recuérdame </label>
+                                </div>
+                            </div>
+                            <div class="col-4">
+                                <div class="d-grid gap-2">
+                                    <button type="submit" class="btn btn-primary" name="entrar">Entrar</button>
+                                </div> 
+        <script
+      src="https://cdn.jsdelivr.net/npm/overlayscrollbars@2.10.1/browser/overlayscrollbars.browser.es6.min.js"
+      integrity="sha256-dghWARbRe2eLlIJ56wNB+b760ywulqK3DzZYEpsg2fQ="
+      crossorigin="anonymous"
+    ></script>
 
+    <script
+      src="https://cdn.jsdelivr.net/npm/@popperjs/core@2.11.8/dist/umd/popper.min.js"
+      integrity="sha384-I7E8VVD/ismYTF4hNIPjVp/Zjvgyol6VFvRkX/vR+Vc4jQkC+hVqc2pM8ODewa9r"
+      crossorigin="anonymous"
+    ></script>
+    <!--end::Required Plugin(popperjs for Bootstrap 5)--><!--begin::Required Plugin(Bootstrap 5)-->
+    <script
+      src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.min.js"
+      integrity="sha384-0pUGZvbkm6XF6gxjEnlmuGrJXVbNuzT9qBBavbLwCsOGabYfZo0T0to5eqruptLy"
+      crossorigin="anonymous"
+    ></script>
+    </body>
 </html>
