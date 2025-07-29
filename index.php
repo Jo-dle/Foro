7e78e0e899e0f229ba96d3098ee5384635070289
neftali <neftaliespinorodriguez@gmail.com>
2025-07-29 08:49:00 +0100

    pruebas iniciales

--- index.php
+++ index.php
@@ -4,10 +4,10 @@ session_start();
 session_regenerate_id(true); // Protege contra fijación de sesión
 
 // Verificar si el usuario está logueado
-//if (!isset($_SESSION['id'])) {
-  //  header("Location: login.php");
-   // exit;
-//}
+if (!isset($_SESSION['id'])) {
+   header("Location: login.php");
+   exit;
+}
 
 // Cerrar sesión si se solicita
 if (isset($_REQUEST["sesion"]) && $_REQUEST["sesion"] === "cerrar") {
